4d85c79f06ded0c6226944077e954417c97aa09f
MothyBran <Timothybran@gmail.com>
2025-07-27 18:41:04 +0200

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,6 @@
+
+document.getElementById("clearFieldsBtn")?.addEventListener("click", () => {
+  document.getElementById("messageCode").value = "";
+  document.getElementById("messageInput").value = "";
+  document.getElementById("messageOutput").value = "";
+});
