96018e02a006cde60b41bd08e35411e0b30e54d2
MothyBran <Timothybran@gmail.com>
2025-07-27 18:41:43 +0200

    Delete public/app.js

--- public/app.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("clearFieldsBtn")?.addEventListener("click", () => {
-  document.getElementById("messageCode").value = "";
-  document.getElementById("messageInput").value = "";
-  document.getElementById("messageOutput").value = "";
-});
