cfa68a5a2f8ac990f996598a8c899cbf4c9e941e
MothyBran <Timothybran@gmail.com>
2025-08-07 04:18:16 +0200

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,7 @@
 // database/db.js
 const { Pool } = require("pg");
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -8,4 +10,33 @@ const pool = new Pool({
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
+};
+
+// Datenbankverbindung
+const db = new sqlite3.Database(path.resolve(__dirname, 'database.sqlite'));
+
+// Benutzer anhand ID laden
+function getUserById(id) {
+  return new Promise((resolve, reject) => {
+    db.get('SELECT * FROM users WHERE id = ?', [id], (err, row) => {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  });
+}
+
+// Lizenz anhand Lizenz-ID laden (aus users.license_key_id)
+function getLicenseById(id) {
+  return new Promise((resolve, reject) => {
+    db.get('SELECT * FROM license_keys WHERE id = ?', [id], (err, row) => {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  });
+}
+
+// Exportieren
+module.exports = {
+  getUserById,
+  getLicenseById,
 };
