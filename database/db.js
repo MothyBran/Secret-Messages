a5b39b523e5a4a4c661b517573ada5c49aba0647
MothyBran <Timothybran@gmail.com>
2025-08-07 04:29:10 +0200

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,5 @@
 // database/db.js
 const { Pool } = require("pg");
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,32 +8,4 @@ const pool = new Pool({
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
-};
-
-// Datenbankverbindung
-const db = new sqlite3.Database(path.resolve(__dirname, '../database.sqlite'));
-
-// Benutzer anhand ID laden
-function getUserById(id) {
-  return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM users WHERE id = ?', [id], (err, row) => {
-      if (err) reject(err);
-      else resolve(row);
-    });
-  });
-}
-
-// Lizenz anhand Lizenz-ID laden
-function getLicenseById(id) {
-  return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM license_keys WHERE id = ?', [id], (err, row) => {
-      if (err) reject(err);
-      else resolve(row);
-    });
-  });
-}
-
-module.exports = {
-  getUserById,
-  getLicenseById
 };
